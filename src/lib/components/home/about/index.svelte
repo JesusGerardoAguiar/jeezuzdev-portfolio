661c9a8ee532693eeea7b476aefcb9e2c7e064a6
Jesus Aguiar <aguiarf.j16@gmail.com>
2023-03-29 21:36:08 -0300

    alexandria text

--- src/lib/components/home/about/index.svelte
+++ src/lib/components/home/about/index.svelte
@@ -7,7 +7,7 @@
 		<img src={about} class="w-[10rem] lg:w-auto  m-auto mb-10"/>
 		<p class="text-black text-[14px] leading-8 font-montserratregular w-[100%] lg:w-[60%] text-center lg:text-start">
 			As a full stack engineer with over 4+ years of experience, I specialize in the <span
-				class="text-secondary font-montserratbold">React ecosystem and Typescript</span>, <br /> My passion for
+				class="text-secondary font-montserratbold">React ecosystem and Typescript</span>, My passion for
 			<span class="text-secondary font-montserratbold">web3 technologies</span>
 			and the potential they hold through decentralization has led me to enroll in various programs and become involved in online communities such as {" "}<span class="text-secondary font-montserratbold"
 				>Buildspace</span
